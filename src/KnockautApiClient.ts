5ac82d86beeee33694341a058d033e85b8e20018
Dario Eberhard <d.eberhard@brelag.com>
2021-07-29 19:12:18 +0200

    add inline comment

--- src/KnockautApiClient.ts
+++ src/KnockautApiClient.ts
@@ -234,7 +234,7 @@ export class KnockautApiClient {
   closeWebSocket() {
     if (this.webSocket !== null) {
       try {
-        /* set to 100, to prevent imediate reconnection in the sockets onClose hook.
+        /* Set to 100, to prevent imediate reconnection in the sockets onClose hook.
         otherwise, socket immediately reconnects if <10 and store is connected. */
         this.reconnectionCount = 100
         this.webSocket.close()
